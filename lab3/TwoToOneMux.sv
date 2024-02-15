9e0aa7e63ac9c034f00e421ea63ee000e4eaf50b
Vinit Patel <122309228+Allicai@users.noreply.github.com>
2024-02-15 10:56:22 -0500

    Create TwoToOneMux.sv

---
+++ lab3/TwoToOneMux.sv
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/15/2024 3:31:33 AM
+// Design Name: 
+// Module Name: PrimaryMux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TwoToOneMux(
+input logic [15:0] a, b,
+input logic sel_mux,
+output logic [15:0] out
+    );
+    always_comb
+        if (sel_mux)
+            out = b;
+        else
+            out = a;
+endmodule
