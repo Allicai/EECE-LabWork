2dee522954b0364c63a21925975c55b1419c0be5
Vinit Patel <122309228+Allicai@users.noreply.github.com>
2024-02-15 20:14:04 -0500

    Add files via upload

---
+++ lab3new/alu.sv
@@ -0,0 +1,88 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/03/2023 11:02:37 AM
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+input logic signed [15:0] a,
+input logic [15:0] b,
+input logic [3:0] s,
+output logic ovf, take_branch,
+output logic [15:0] f
+    );
+    
+    always_comb
+        case(s)
+            0 : begin
+                   f = a + b;
+                   take_branch = 0;
+                   if ((a[15] & b[15] & ~f[15]) | (~a[15] & ~b[15] & f[15]))
+                        ovf = 1;
+                   else
+                        ovf = 0;
+                end
+            1 : begin
+                    f = ~b;
+                    take_branch = 0;
+                    ovf = 0;
+                end
+            2 : begin
+                    f = a & b;
+                    take_branch = 0;
+                    ovf = 0;
+                end
+            3 : begin
+                    f = a | b;
+                    take_branch = 0;
+                    ovf = 0;
+                end
+            4 : begin
+                    f = a >>> b;
+                    take_branch = 0;
+                    ovf = 0;
+                end
+            5 : begin
+                    f = a << b;
+                    take_branch = 0;
+                    ovf = 0;
+                end
+            6 : begin
+                    f = 0;
+                    if (a == 0)
+                        take_branch = 1;
+                    else
+                        take_branch = 0;
+                    ovf = 0;
+                end
+            7 : begin
+                    f = 0;
+                    if (a != 0)
+                        take_branch = 1;
+                    else
+                        take_branch = 0;
+                    ovf = 0;
+                end
+            default : begin
+                        f = a ^ b;
+                        take_branch = 0;
+                        ovf = 0;
+                    end
+            endcase
+endmodule
---
+++ lab3new/reg_file.sv
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/03/2023 10:25:49 AM
+// Design Name: 
+// Module Name: reg_file
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module regfile(
+input logic rst, clk, wr_en,
+input logic [2:0] rd0_addr, rd1_addr, wr_addr,
+input logic [15:0] wr_data,
+output logic [15:0] rd0_data, rd1_data);
+
+    reg [15:0] mem [0:7];
+    always @ (posedge clk)
+    begin
+        if (rst)
+            for (int i = 0; i < 16; i = i + 1)
+                begin
+                    mem[i] <= 0;
+                end
+       else if (wr_en)
+        mem[wr_addr] = wr_data;
+       else if (!wr_en)
+        rd0_data = mem[rd0_addr];
+        rd1_data = mem[rd1_addr];
+    end
+    
+endmodule
