59ce9e1dbfb1540c78779c44c1d9ae6cf602f678
Vinit Patel <122309228+Allicai@users.noreply.github.com>
2024-02-15 20:14:19 -0500

    Update PrimaryMux.sv

--- lab3new/PrimaryMux.sv
+++ lab3new/PrimaryMux.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/03/2023 10:59:33 AM
-// Design Name: 
-// Module Name: PrimaryMux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module TwoToOneMux(
