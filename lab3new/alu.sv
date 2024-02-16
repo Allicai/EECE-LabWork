af2f3d20ac097c76c99f88b4664537df6e33205d
Vinit Patel <122309228+Allicai@users.noreply.github.com>
2024-02-15 20:14:35 -0500

    Update alu.sv

--- lab3new/alu.sv
+++ lab3new/alu.sv
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/03/2023 11:02:37 AM
-// Design Name: 
-// Module Name: alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module alu(
