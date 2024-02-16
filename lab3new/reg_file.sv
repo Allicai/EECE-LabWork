4bc82f740dbc7c1d81c89075a4ff156144d2e9bf
Vinit Patel <122309228+Allicai@users.noreply.github.com>
2024-02-15 20:14:57 -0500

    Update reg_file.sv

--- lab3new/reg_file.sv
+++ lab3new/reg_file.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/03/2023 10:25:49 AM
-// Design Name: 
-// Module Name: reg_file
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module regfile(
