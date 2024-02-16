04c2de43118de77683aecd200e641d5c201688f2
Vinit Patel <122309228+Allicai@users.noreply.github.com>
2024-02-15 20:14:27 -0500

    Update ZeroMux.sv

--- lab3new/ZeroMux.sv
+++ lab3new/ZeroMux.sv
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/03/2023 10:56:34 AM
-// Design Name: 
-// Module Name: ZeroMux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module ZeroMux(
