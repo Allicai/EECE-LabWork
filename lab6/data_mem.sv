7c63a8a95b4c37efbbecf624acd6e7bbbd580594
Vinit Patel <122309228+Allicai@users.noreply.github.com>
2024-03-25 22:51:05 -0400

    Create data_mem.sv

---
+++ lab6/data_mem.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/22/2024 08:20:03 PM
+// Design Name: 
+// Module Name: data_mem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module data_mem(
+
+    );
+endmodule
